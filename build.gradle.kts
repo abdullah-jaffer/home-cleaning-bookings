f7459f2922edc42e4a2ccdb54efa876bdbc1bfbb
abdullahjaffer <abdullah@jillionpay.com>
2024-06-26 23:36:44 +0500

    setup PostGress, Liquibase and docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,9 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.liquibase:liquibase-core")
+	implementation("org.postgresql:postgresql:42.7.3")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
