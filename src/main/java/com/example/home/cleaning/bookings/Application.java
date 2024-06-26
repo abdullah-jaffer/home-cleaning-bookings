2159c3cbcdc15b4893d8c59cce39f37e0f15e5fe
abdullahjaffer <abdullah@jillionpay.com>
2024-06-26 22:39:43 +0500

    Initial commit

---
+++ src/main/java/com/example/home/cleaning/bookings/Application.java
@@ -0,0 +1,13 @@
+package com.example.home.cleaning.bookings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
