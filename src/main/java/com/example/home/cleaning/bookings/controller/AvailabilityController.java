110fd3f876d76799350412e9bb1631c7a0bd3832
abdullahjaffer <abdullah@jillionpay.com>
2024-06-30 00:46:31 +0500

    Added example for AvailabilityController request parameters

--- src/main/java/com/example/home/cleaning/bookings/controller/AvailabilityController.java
+++ src/main/java/com/example/home/cleaning/bookings/controller/AvailabilityController.java
@@ -2,6 +2,7 @@ package com.example.home.cleaning.bookings.controller;
 
 import com.example.home.cleaning.bookings.dto.response.AvailabilityResponse;
 import com.example.home.cleaning.bookings.service.AvailabilityService;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,11 +27,14 @@ public class AvailabilityController {
     public List<AvailabilityResponse> getAvailabilities(
             @RequestParam
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+            @Parameter(description = "Date in ISO format (yyyy-MM-dd)", required = true, example = "2024-07-01")
             LocalDate date,
             @RequestParam(required = false)
             @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+            @Parameter(description = "Time in ISO format (HH:mm:ss)", example = "14:30:00")
             LocalTime time,
             @RequestParam(required = false)
+            @Parameter(description = "Number of hours", example = "2")
             Integer hours) {
         return this.availabilityService.getAvailabilities(date, time, hours);
     }
